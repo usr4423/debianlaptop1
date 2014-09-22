a60902841229450d145426088915abbb53427157
arch <arch@debian>
2014-09-23 02:57:38 +0900

    log

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,16 +1,58 @@
 import XMonad
-import XMonad.Config.Gnome
-import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig
-myManageHook = composeAll (
-[ manageHook gnomeConfig
-])
-main = xmonad $ gnomeConfig {
-manageHook = myManageHook,
-modMask = mod4Mask
+import XMonad.Actions.WindowGo
+import XMonad.Util.EZConfig --removekeys, additionalkeys
+import XMonad.Util.Run
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks --spawnPipe, hPutStrLn
+
+main = do
+     myStatusBar <- spawnPipe "xmobar"
+     xmonad $ defaultConfig{
+      terminal = myTerminal
+     , modMask = myModMask
+     , borderWidth = myBorderWidth
+     , normalBorderColor = "#99ccff"
+     , focusedBorderColor = "#0033dd"
+     , layoutHook = myLayoutHook
+     , manageHook = myManageHook
+     , logHook = myLogHook myStatusBar
+     }
+
+     -- `removeKeys` myOverriddenKeys
+     -- `additionalKeys` myAdditionalKeys
+
+myTerminal      =       "konsole"
+myModMask       =       mod4Mask
+myBorderWidth   =       2
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+myManageHook = manageDocks <+> manageHook defaultConfig
+
+myLogHook h = dynamicLogWithPP xmobarPP {
+ppOutput = hPutStrLn h
 }
-`additionalKeysP`
-[ ("M-x k", spawn "kupfer")
-, ("M-x f", spawn "firefox")
-, ("M-x e", spawn "emacs")
-]
+
+-- myFont = "monaco:size=6"
+
+
+-- myAdditionalKeys = [
+-- -- launch dmenu
+-- -- ((myModMask,xK_p ), spawn ("dmenu_run -fn '" ++ myFont ++ "' -nf grey"))
+
+-- -- Custom commands
+-- , ((myModMask,xK_c ), spawn "firefox")
+-- ]
+
+-- -- myOverriddenKeys = [
+-- -- (myModMask,xK_p )
+-- -- ]
+
+
+-- for keyboard shortcut
+-- modm = mod4Mask
+-- `additionalKeys`=
+-- [
+-- ((modm,xK_e),spawn "emacs"),
+-- ((modm,xK_t),spawn "konsoke"),
+-- ((modm,xK_F4),kill)
+-- ((modm,xK_f),spawn "firefox")
+-- ]
