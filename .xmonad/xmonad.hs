369bc22259a9261cc1b0497e958bec8747ea0283
arch <arch@debian>
2014-09-22 23:14:49 +0900

    a

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,28 +1,16 @@
 import XMonad
-import XMonad.Actions.WindowGo
+import XMonad.Config.Gnome
+import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
-
-main = do
-     xmonad $ defaultConfig{
-      terminal = myTerminal
-     , modMask = myModMask
-     , borderWidth = myBorderWidth
-     , normalBorderColor = "#99ccff"
-     , focusedBorderColor = "#0033dd"
-     }
-
-
-myTerminal      =       "konsole"
-myModMask       =       mod4Mask
-myBorderWidth   =       2
-
-
---for keyboard shortcut
--- modm = mod4Mask
--- let `additionalKeys`
--- [
--- ((modm,xK_e),spawn "emacs"),
--- ((modm,xK_t),spawn "konsoke"),
--- ((modm,xK_F4),kill)
--- ((modm,xK_f),spawn "chromium")
--- ]
+myManageHook = composeAll (
+[ manageHook gnomeConfig
+])
+main = xmonad $ gnomeConfig {
+manageHook = myManageHook,
+modMask = mod4Mask
+}
+`additionalKeysP`
+[ ("M-x k", spawn "kupfer")
+, ("M-x f", spawn "firefox")
+, ("M-x e", spawn "emacs")
+]
